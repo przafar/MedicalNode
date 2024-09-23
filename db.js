d18c3b23f6d2acdaaa7ca3ebf4c334b30eba1075
Zafar <su10409@studenti.polito.it>
2024-09-24 00:25:09 +0500

    s

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
-const { Pool } = require('pg')
-
-const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'Rasulbek',
-    database: 'test_db'
-})
-
-module.exports = pool
+PORT=3001
+ACCESS_TOKEN_SECRET=your_secret_key
+DB_HOST=localhost
+DB_PORT=5432
+DB_USER=postgres
+DB_PASSWORD=Rasulbek
+DB_NAME=test_db
