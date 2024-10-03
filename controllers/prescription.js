9ae4b8756219da3c4319fc41f76f2078c6b929c2
Zafar <su10409@studenti.polito.it>
2024-10-03 18:29:45 +0500

    ref pdf

--- controllers/prescription.js
+++ controllers/prescription.js
@@ -20,9 +20,93 @@ const authenticateToken = (req, res, next) => {
   });
 };
 
+const replaceTemplateVariables = (template, data, appointment_info) => {
+  return template
+    .replace('{{doctorName}}', data.prescribing_doctor)
+    .replace('{{doctorSpecialty}}', appointment_info.encounter_class_display || 'Стоматолог')
+    .replace('{{patientName}}', appointment_info.patient_name)
+    .replace('{{date}}', new Date(data.created_at).toLocaleDateString())
+    .replace('{{birthDate}}', new Date(appointment_info.patient_birth_date).toLocaleDateString() || '_________________________')
+    .replace('{{notes}}', data.notes || '_________________________')
+    .replace('{{dischargeDate}}', new Date(appointment_info.updated_at).toLocaleString() || '_________________________')
+    .replace('{{medications}}', data.medications.map(med => `
+      <tr>
+        <td>${med.name}</td>
+        <td>${med.dosage}</td>
+        <td>${med.duration}</td>
+        <td>${med.frequency}</td>
+      </tr>
+    `).join(''));
+};
+
+const generatePrescriptionPDF = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const prescriptionResult = await pool.query('SELECT * FROM prescriptions WHERE id = $1', [id]);
+    if (prescriptionResult.rows.length === 0) {
+      return res.status(404).send({ message: "Prescription not found" });
+    }
+
+    const prescription = prescriptionResult.rows[0];
+    if (typeof prescription.medications === 'string') {
+      prescription.medications = JSON.parse(prescription.medications);
+    }
+
+    const appointmentResult = await pool.query(`
+      SELECT 
+        appointments.*, 
+        CONCAT(patients.last_name, ' ', patients.first_name, ' ', patients.middle_name) AS patient_name,
+        patients.birth_date AS patient_birth_date,
+        encounter_classes.display AS encounter_class_display,
+        encounter_types.display AS encounter_type_display
+      FROM appointments 
+      JOIN patients ON appointments.patient_id = patients.id
+      LEFT JOIN encounter_classes ON appointments.encounter_class = encounter_classes.code
+      LEFT JOIN encounter_types ON appointments.encounter_type = encounter_types.code
+      WHERE appointments.id = $1`, [prescription.appointment_id]);
+
+    if (appointmentResult.rows.length === 0) {
+      return res.status(404).send({ message: "Appointment not found" });
+    }
+
+    const appointment_info = appointmentResult.rows[0];
+
+    const templatePath = path.join(__dirname, '../templates/prescriptionTemplate.html');
+    console.log('Template Path:', templatePath);
+    let htmlTemplate = fs.readFileSync(templatePath, 'utf8');
+
+    htmlTemplate = replaceTemplateVariables(htmlTemplate, prescription, appointment_info);
+    console.log('Generated HTML:', htmlTemplate);
+
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+
+    const page = await browser.newPage();
+    await page.setContent(htmlTemplate, { waitUntil: 'networkidle0' });
+
+    const pdf = await page.pdf({
+      format: 'A4',
+      printBackground: true,
+    });
+
+    await browser.close();
+
+    console.log('PDF successfully generated');
+
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', `attachment; filename=prescription-${prescription.id}.pdf`);
+    res.end(pdf);
+  } catch (error) {
+    console.error("Error generating prescription PDF:", error);
+    res.status(500).send({ error: error.message });
+  }
+};
+
 const getPrescriptions = async (req, res) => {
   const { page = 1, per_page = 10, appointment_id } = req.query;
-
   const pageInt = parseInt(page, 10);
   const perPageInt = parseInt(per_page, 10);
   const offset = (pageInt - 1) * perPageInt;
@@ -82,11 +166,7 @@ const getPrescriptionById = async (req, res) => {
   const id = parseInt(req.params.id);
 
   try {
-    const result = await pool.query(
-      `SELECT * FROM prescriptions WHERE id = $1`,
-      [id]
-    );
-
+    const result = await pool.query('SELECT * FROM prescriptions WHERE id = $1', [id]);
     if (result.rows.length === 0) {
       return res.status(404).send({ message: "Prescription not found" });
     }
@@ -144,7 +224,7 @@ const deletePrescription = async (req, res) => {
   const id = parseInt(req.params.id);
 
   try {
-    const result = await pool.query(`DELETE FROM prescriptions WHERE id = $1 RETURNING *`, [id]);
+    const result = await pool.query('DELETE FROM prescriptions WHERE id = $1 RETURNING *', [id]);
 
     if (result.rowCount === 0) {
       return res.status(404).send({ message: "Prescription not found" });
@@ -156,91 +236,6 @@ const deletePrescription = async (req, res) => {
     res.status(500).send({ error: error.message });
   }
 };
-
-const replaceTemplateVariables = (template, data, appointment_info) => {
-  return template
-    .replace('{{doctorName}}', data.prescribing_doctor)
-    .replace('{{doctorSpecialty}}', appointment_info.encounter_class_display || 'Стоматолог')
-    .replace('{{patientName}}', appointment_info.patient_name)
-    .replace('{{date}}', new Date(data.created_at).toLocaleDateString())
-    .replace('{{birthDate}}', new Date(appointment_info.patient_birth_date).toLocaleDateString() || '_________________________')
-    .replace('{{notes}}', data.notes || '_________________________')
-    .replace('{{dischargeDate}}', new Date(appointment_info.updated_at).toLocaleString() || '_________________________')
-    .replace('{{medications}}', data.medications.map(med => `
-      <tr>
-        <td>${med.name}</td>
-        <td>${med.dosage}</td>
-        <td>${med.duration}</td>
-        <td>${med.frequency}</td>
-      </tr>
-    `).join(''));
-};
-
-const generatePrescriptionPDF = async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const prescriptionResult = await pool.query('SELECT * FROM prescriptions WHERE id = $1', [id]);
-    if (prescriptionResult.rows.length === 0) {
-      return res.status(404).send({ message: "Prescription not found" });
-    }
-
-    const prescription = prescriptionResult.rows[0];
-
-    if (typeof prescription.medications === 'string') {
-      prescription.medications = JSON.parse(prescription.medications);
-    }
-
-    const appointmentResult = await pool.query(
-      `SELECT 
-        appointments.*, 
-        CONCAT(patients.last_name, ' ', patients.first_name, ' ', patients.middle_name) AS patient_name,
-        patients.birth_date AS patient_birth_date,
-        encounter_classes.display AS encounter_class_display,
-        encounter_types.display AS encounter_type_display
-      FROM appointments 
-      JOIN patients ON appointments.patient_id = patients.id
-      LEFT JOIN encounter_classes ON appointments.encounter_class = encounter_classes.code
-      LEFT JOIN encounter_types ON appointments.encounter_type = encounter_types.code
-      WHERE appointments.id = $1`,
-      [prescription.appointment_id]
-    );
-
-    if (appointmentResult.rows.length === 0) {
-      return res.status(404).send({ message: "Appointment not found" });
-    }
-
-    const appointment_info = appointmentResult.rows[0];
-
-    const templatePath = path.join(__dirname, '../templates/prescriptionTemplate.html');
-    let htmlTemplate = fs.readFileSync(templatePath, 'utf8');
-
-    htmlTemplate = replaceTemplateVariables(htmlTemplate, prescription, appointment_info);
-
-    const browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
-    const page = await browser.newPage();
-
-    await page.setContent(htmlTemplate, { waitUntil: 'networkidle0' });
-
-    const pdf = await page.pdf({
-      format: 'A4',
-      printBackground: true,
-    });
-
-    await browser.close();
-
-    res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', `attachment; filename=prescription-${prescription.id}.pdf`);
-    res.end(pdf);
-  } catch (error) {
-    console.error("Error generating prescription PDF:", error);
-    res.status(500).send({ error: error.message });
-  }
-};
-
 
 module.exports = {
   getPrescriptions,
